d9625b204f6274b050fa83df693f77d0e996a90c
Gage <gagemigan@gmail.com>
2020-06-29 21:08:52 -0400

    ay got work :3

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
-'use strict';
+
+
+
+
+const breedStore = [];
+
+
+const getBreeds = () =>{
+  fetch('https://dog.ceo/api/breeds/list/all')
+    .then((res) =>{
+      return res.json();
+    })
+    .catch((error) => {
+      throw new Error(error);
+    })
+    .then( data =>{
+      let res = [];
+      for(let i in data.message){
+        res.push(`<option value="${i}">${i}</option>`);
+      }
+      res = res.join(' \n ');
+      $('#breeds').html(res);
+    });
+};
 
 function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+  const breed = $('#breeds').val();
+  const count = $('#dogcount').val();
+  const url= `https://dog.ceo/api/breed/${breed}/images/random/${count}`;
+  
+  fetch(url)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+
+
 function displayResults(responseJson) {
-  console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
-  //display the results section
-  $('.results').removeClass('hidden');
+  if($('#dogcount').val() === 1){
+    $('.results-img').replaceWith(
+      `<img src="${responseJson.message}" class="results-img">`
+    );
+    //display the results section
+    $('.results').removeClass('hidden');
+  }else{
+    const res =[];
+    responseJson.message.map( dog => res.push(`<img src="${dog}" class="results-img">`));
+    $('.results-img').replaceWith(res.join(''));
+    $('.results').removeClass('hidden');
+
+  }
+  
 }
+
+
 
 function watchForm() {
   $('form').submit(event => {
@@ -25,7 +63,9 @@ function watchForm() {
   });
 }
 
-$(function() {
-  console.log('App loaded! Waiting for submit!');
+function main (){
+  getBreeds();
   watchForm();
-});
+}
+
+$(main);
